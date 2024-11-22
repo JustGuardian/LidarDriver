af20a01f66d8c725fd5cccfe8aec377dc8124b8a
Steva0 <michele.stevanin@gmail.com>
2024-11-22 17:34:36 +0100

    creata intestazioni metodi in LidarDriver.h

--- include/LidarDriver.h
+++ include/LidarDriver.h
@@ -2,12 +2,18 @@
 #define LIDARDRIVER_H
 
 #include "CircularArray.h"
+#include <vector>
+#include <iostream>
 
 class LidarDriver{
 private:
     CircularArray array;
 public:
-
+    LidarDriver(int x = 10);       //costruttore
+    void new_scan(std::vector<double>); //inserisce un nuovo vettore
+    std::vector<double> get_scan();     //restituisce vettore più vecchio e lo rimuove dal buffer
+    void clear_buffer();        //svuota il buffer
+    double get_distance(double);    //resituisce la distanza del vettore più recente di quel relativo angolo
 };
 
 #endif //!LIDARDRIVER_H
