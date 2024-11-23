d6c8e04c5489ce7845dd94aad5e69dc37749437e
Steva0 <148330069+Steva0@users.noreply.github.com>
2024-11-23 13:54:37 +0100

    Update LidarDriver.h
    Aggiunta intestazione metodo adjust_scan_size da poi inserire in new scan

--- include/LidarDriver.h
+++ include/LidarDriver.h
@@ -10,6 +10,7 @@
 class LidarDriver{
 private:
     CircularArray array;
+    std::vector<double> adjust_scan_size(std::vector<double>) const;    //controlla il numero di elementi della lista da inserire (se size<181 riempie con 0 fino a 181, se size > 1810 taglia gli elementi dopo)
 public:
     //costruttori
     LidarDriver(int x = 10);       //costruttore di default
@@ -21,12 +22,12 @@ public:
     std::vector<double> latest_scan() const;    //restituisce vettore più nuovo senza rimuoverlo dal buffer
     void clear_buffer();        //svuota il buffer
     double get_distance(double) const;    //resituisce la distanza del vettore più recente di quel relativo angolo
-    bool is_buffer_empty() const;
-    bool is_buffer_full() const;
+    bool is_buffer_empty() const;    //restituisce 1 se è vuoto
+    bool is_buffer_full() const;    //restituisce 1 se è pieno
 
     // Dichiarazione dell'operatore di stampa come friend
-    void print_all_scans() const;
-    friend std::ostream& operator<<(std::ostream& os, const LidarDriver& driver);
+    void print_all_scans() const;    //metodo per stampare tutto il buffer
+    friend std::ostream& operator<<(std::ostream& os, const LidarDriver& driver);     //overloading dell'operatore di stampa (stampa l'ultima lista dati inserita)
 };
 
 #endif //!LIDARDRIVER_H
